5e64297641c4159558961ae9009003f315b82bec
jhughe70 <jhughe70@uncc.edu>
2023-09-18 15:52:11 -0400

    Added webpage functionality for a homepage

--- EpilepsyWebsite/EpiWeb/src/main/java/com/example/EpiWeb/controller/HomeController.java
+++ EpilepsyWebsite/EpiWeb/src/main/java/com/example/EpiWeb/controller/HomeController.java
@@ -3,8 +3,13 @@ package com.example.EpiWeb.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     
+    @RequestMapping("/home")
+    public String home() {
+        return "home.html";
+    }
 }
