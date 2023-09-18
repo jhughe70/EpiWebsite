1b288f094e5a65da297fa4cc11ffaf035682c727
jhughe70 <jhughe70@uncc.edu>
2023-09-18 15:15:29 -0400

    Initial Commit

---
+++ EpilepsyWebsite/EpiWeb/src/main/java/com/example/EpiWeb/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.example.EpiWeb.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    
+}
---
+++ EpilepsyWebsite/EpiWeb/src/test/java/com/example/EpiWeb/EpiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EpiWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
